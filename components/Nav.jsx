281d81fe5ac0ea707efe09975bf064d4cbfe35bd
munyi-victor <munyivictor6@gmail.com>
2024-01-26 16:18:03 +0300

    first update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,8 +2,21 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="w-full flex-col pt-6 justify-between shadow-lg min-h-12">
-      <Link href="/">Home</Link>
+    <nav className="w-full flex-col pt-2 pl-10 justify-between shadow-lg min-h-12">
+      <div className="flex gap-4">
+        <Link
+          href="/"
+          className="bg-light text-black text-lg font-bold font-satoshi rounded-full"
+        >
+          Home
+        </Link>
+        <Link
+          href="/create"
+          className="bg-light text-black text-lg font-bold font-satoshi rounded-full"
+        >
+          Create
+        </Link>
+      </div>
     </nav>
   );
 };
